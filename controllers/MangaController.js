f480825f3637c0fc6e29b5bda8fbac0464a9b1d7
@nandawardi <nanda@ecampus.ut.ac.id>
2021-04-07 13:14:09 +0700

    Implement response, init puppeteer multiple sites

--- controllers/MangaController.js
+++ controllers/MangaController.js
@@ -1,79 +1,110 @@
 const axios = require('axios').default;
 const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
+const responses = require('../helpers/responses');
 
 class MangaController {
 
   static async index(req, res) {
-    const browser =  await puppeteer.launch({
-      headless: true,
-    })
+    try {
+      const urls = [
+        `https://www.tokopedia.com/search?navsource=home&sc=3309&source=universe&st=product&q=${req.params.name.replace(/\s/g, '%20')}`,
+        `https://www.bukalapak.com/c/hobi-koleksi/buku/komik?search%5Bkeywords%5D=${req.params.name.replace(/\s/g, '%20')}`,
+        `https://shopee.co.id/search?facet=16895%2C16893&keyword=${req.params.name.replace(/\s/g, '%20')}`
+      ];
+  
+      const browser =  await puppeteer.launch({
+        headless: true,
+      });
+  
+      const page = await browser.newPage();
+      page.setViewport({
+        width: 1366,
+        height: 768
+      });
+  
+      const obj = {};
+      // urls.forEach(async (i, e) => {
+        
+      // })
+      for (let i = 0; i < urls.length; i++) {
+        await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36");
+        await page.goto(urls[i], {
+          waitUntil: 'domcontentloaded'
+        });
+  
+        await page.evaluate('window.scrollTo(0,99999)');
+        await page.waitForTimeout(1000);
+        const content = await page.content();
+        const $ = cheerio.load(content);
+        
+        if (page.url().includes("tokopedia")) {
+          let tokopedia = [];
+          const product = $(".css-1g20a2m");
+          product.each((_, e) => {
+            let content_obj = {};
+            const link = $(e)
+              .find(".css-1ehqh5q > a")
+              .attr("href")
+    
+            const title = $(e)
+              .find(".css-7fmtuv > a > .css-18c4yhp")
+              .text()
+              .trim();
+  
+            const price = $(e)
+              .find(".css-7fmtuv > a > .css-rhd610")
+              .text()
+              .trim()
+  
+            const location = $(e)
+              .find(".css-7fmtuv > a > .css-vogbyu > .css-9dkp4i > .css-1pznt2j > span[class='css-4pwgpi']")
+              .map((i, v) => {
+                let store_location = $(v).text().trim()
+                return store_location
+              })
+              .get(0);
+  
+            const store_name = $(e)
+              .find(".css-7fmtuv > a > .css-vogbyu > .css-9dkp4i > .css-1pznt2j > span[class='css-4pwgpi']")
+              .map((i, v) => {
+                let name = $(v).text().trim()
+                return name
+              })
+              .get(1);
+  
+            content_obj.product = Object({
+              name: title,
+              price
+            })
+  
+            content_obj.link = link;
+  
+            content_obj.store = Object({
+              name: store_name,
+              location
+            })
+            tokopedia.push(content_obj)
+          });
+          obj.tokopedia = tokopedia;
+        }
 
-    const page = await browser.newPage();
-    page.setViewport({
-      width: 1366,
-      height: 768
-    });
-    await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36");
-    await page.goto(`https://www.tokopedia.com/search?navsource=home&sc=3309&source=universe&st=product&q=${req.params.name.replace(/\s/g, '%20')}`, {
-      waitUntil: 'domcontentloaded'
-    });
+        if (page.url().includes("shopee")) {
+          let shopee = [];
+          obj.shopee = shopee;
+        }
 
-    await page.evaluate('window.scrollTo(0,99999)');
-    await page.waitForTimeout(1000);
-    const content = await page.content();
-    const $ = cheerio.load(content);
-
-    // const request = await axios.get("https://www.tokopedia.com/search?navsource=home&sc=3309&source=universe&st=product&q=yokohama%20shopping%20blog");
-    // const $ = cheerio.load(request.data);
-
-    const product = $(".css-1g20a2m");
-    product.each((i, e) => {
-      const links = $(e)
-        .find(".css-1ehqh5q > a")
-        .attr("href")
-      links_arr.push(links);
-
-      const title = $(e)
-        .find(".css-7fmtuv > a > .css-18c4yhp")
-        .text()
-        .trim();
-      
-      title_arr.push(title_arr);
-
-      const price = $(e)
-        .find(".css-7fmtuv > a > .css-rhd610")
-        .text()
-        .trim()
-
-      price_arr.push(price);
-
-      const location = $(e)
-        .find(".css-7fmtuv > a > .css-vogbyu > .css-9dkp4i > .css-1pznt2j > span[class='css-4pwgpi']")
-        .map((i, v) => {
-          let store_location = $(v).text().trim()
-          return store_location
-        })
-        .get(0);
-      
-      location_arr.push(location);
-
-      const store_name = $(e)
-        .find(".css-7fmtuv > a > .css-vogbyu > .css-9dkp4i > .css-1pznt2j > span[class='css-4pwgpi']")
-        .map((i, v) => {
-          let name = $(v).text().trim()
-          return name
-        })
-        .get(1);
-
-      store_arr.push(store_name);
-    });
-    obj.links = links_arr;
-    obj.price = price_arr;
-    obj.title = title_arr;
-    obj.location = location_arr;
-    obj.store = store_arr;
-    return res.send(obj);
+        if (page.url().includes("bukalapak")) {
+          let bukalapak = [];
+          obj.bukalapak = bukalapak;
+        }
+        
+      }
+  
+      return res.status(200).json(responses("200", "Success", obj));
+    } catch (error) {
+      return res.status(400).json(responses("400", error.message))
+    }
   }
 
   static async getStore(req, res) {
