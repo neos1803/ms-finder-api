a61e73e1473f2f3133d628574a07ebf2fe0e76ac
Ardi <oliviergiroud1998@gmail.com>
2021-04-12 13:59:59 +0700

    Configure swagger

--- index.js
+++ index.js
@@ -2,6 +2,33 @@ require('dotenv').config();
 
 const express = require('express');
 const mangaRoute = require('./routes/manga');
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerDefinition =  {
+  openapi: "3.0.0",
+  info: {
+    title: "Ecommerce Web Scraping API",
+    version: "1.0.0",
+    description: "This is an api to scrape several local ecommerce to get info about manga product",
+    license: {
+      name: "Licensed Under MIT",
+      url: "https://spdx.org/licenses/MIT.html",
+    },
+    contact: {
+      name: "Purba",
+      url: "https://twitter.com/_NandaPurba_",
+      email: "nandawardipurba@gmail.com",
+    },
+  }
+};
+
+const options = {
+  swaggerDefinition,
+  apis: ["./routes/*.js"]
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 const app = express();
 
@@ -11,6 +38,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/', mangaRoute);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+  explorer: true
+}));
 
 app.get('/', (req, res) => {
   res.json(`It's up`);
