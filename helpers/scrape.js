7d35e53da37484e1bd64317a46b53ceb7e439eeb
Ardi <oliviergiroud1998@gmail.com>
2021-04-12 20:47:11 +0700

    Create scrape class

--- helpers/scrape.js
+++ helpers/scrape.js
@@ -1,12 +1,36 @@
-function scrapeTextOrAttr ($, e, find, attr = Boolean, attrValue = null) {
-  if (attr) {
+class Scrape {
+  static scrapeTextOrAttr ($, e, find, attr = false, attrValue = null) {
+    if (attr) {
+      return $(e)
+        .find(find)
+        .attr(attrValue)
+    }
     return $(e)
       .find(find)
-      .attr(attrValue)
+      .text()
   }
-  return $(e)
-    .find(find)
-    .text()
+  
+  static async autoScroll(page) {
+    await page.evaluate(async () => {
+      await new Promise((resolve, reject) => {
+        let total_height = 0;
+        let distance = 100;
+  
+        const timer = setInterval(() => {
+          const scrollHeight = document.body.scrollHeight;
+          window.scrollBy(0, distance);
+          total_height+=distance;
+  
+  
+          if (total_height >= scrollHeight) {
+            clearInterval(timer),
+            resolve()
+          }
+        }, 400);
+      });
+    });
+  }
+
 }
 
-module.exports = scrapeTextOrAttr;
+module.exports = Scrape;
