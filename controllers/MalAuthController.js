d0ccf756f6b1920eebdf07e6a742d6b39365a0a6
Ardi <oliviergiroud1998@gmail.com>
2021-04-22 13:24:33 +0700

    Add controller to handle MAL authentication

--- controllers/MalAuthController.js
+++ controllers/MalAuthController.js
@@ -1,6 +1,8 @@
 const puppeteer = require("puppeteer");
 const crypto =  require("crypto");
 const responses = require("../helpers/responses");
+const querystring = require("querystring");
+const axios = require("axios").default;
 require("dotenv").config();
 
 class MalAuthController {
@@ -36,16 +38,35 @@ class MalAuthController {
       await page.goto(url, {
         waitUntil: 'domcontentloaded'
       });
-      await page.waitForSelector("#content > div > div > div > form > input.submit-block__button.button.button--primary.btn-recaptcha-submit");
+      await page.waitForSelector("#content > div > div > div > form > input.submit-block__button.button.button--primary.btn-recaptcha-submit", {
+        timeout: 100000
+      });
       await page.click("#content > div > div > div > form > input.submit-block__button.button.button--primary.btn-recaptcha-submit");
       await page.waitForTimeout(1000);
       await page.waitForNavigation();
 
-      const url_code = page.url().replace("https://twitter.com/_NandaPurba_?", "").replace("&state=RequestID42", "").replace("code=", "")
+      const url_code = page.url().replace("https://twitter.com/_NandaPurba_?", "").replace("&state=RequestID42", "").replace("code=", "");
+
+      await page.close();
+      await browser.close();
+
+      const config = {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      };
       
-      return res.status(200).json(responses("200", "Success", { url_code }));
+      const auth = await axios.post('https://myanimelist.net/v1/oauth2/token', querystring.stringify({
+        'client_id': process.env.CLIENT_ID,
+        'client_secret': process.env.CLIENT_SECRET,
+        'code': url_code,
+        'code_verifier': code_verifier,
+        'grant_type': 'authorization_code'
+      }), config);
+      
+      return res.status(200).json(responses("200", "Success", auth.data));
     } catch (error) {
-      return res.status(500).json(responses("500", error.message, null));
+      return res.status(500).json(responses("500", error.message, { error }));
     }
   }
 
